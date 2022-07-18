f380bc6231e72ee7fbdd67b7d90793bef8fc567d
kevincobain2000 <kevincobain2000@users.noreply.github.com>
2022-07-18 10:36:15 +0000

    deploy: 2995ae982bb24773f3c68fcc769ed2b3aa29d7e9

---
+++ _next/static/lBXx_pTGMpvuZWBEIc9eH/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-6e5b8dc21df646c5.js"],"/_error":["static/chunks/pages/_error-9e0ee9ed17c97d02.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()

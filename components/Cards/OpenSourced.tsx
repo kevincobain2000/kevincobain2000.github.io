04c5e043cda95415ce2c8c15184dbb65c577914a
Pulkit Kathuria <kevincobain2000@gmail.com>
2022-07-19 22:42:57 +0900

    json

--- components/Cards/OpenSourced.tsx
+++ components/Cards/OpenSourced.tsx
@@ -1,193 +1,116 @@
 interface Props {
 }
 export const OpenSourced = (props:Props) => {
+    const openSourced = [
+        {
+            name: "Sentiment Classifier",
+            description: "Sentiment Classifier using Word Sense Disambiguation",
+            github: "https://github.com/kevincobain2000/sentiment_classifier",
+            demo: "",
+            blog: "",
+        },
+        {
+            name: "jProcessing",
+            description: "Japanese Natural Langauge Processing Libraries",
+            github: "https://github.com/kevincobain2000/jProcessing",
+            demo: ""
+        },
+        {
+            name: "Laravel Throttled Alert Notifications",
+            description: "Send alert to email, microsoft teams from laravel app, when an exception occurs.",
+            github: "https://github.com/kevincobain2000/laravel-alert-notifications",
+            demo: ""
+        },
+        {
+            name: "Gobrew",
+            description: "Install and manage Go version",
+            github: "https://github.com/kevincobain2000/gobrew",
+            demo: "https://github.com/kevincobain2000/gobrew/actions",
+            blog: "https://medium.com/web-developer/go-version-manager-gobrew-c8750157dfe6"
+        },
+        {
+            name: "Setup Go - Github Action",
+            description: "Setup Go in github action",
+            github: "https://github.com/kevincobain2000/action-gobrew",
+            demo: "",
+            blog: "https://medium.com/web-developer/dead-simple-go-setup-github-actions-ee9fd7e1c87f"
+        },
+        {
+            name: "Scrape App store and Google play reviews",
+            description: "App store reviews monitoring tool",
+            github: "https://github.com/kevincobain2000/go-app-reviews-scraper",
+            demo: "",
+            blog: ""
+        },
+        {
+            name: "Laravel Request Docs",
+            description: "Automatically generate Laravel API docs",
+            github: "https://github.com/rakutentech/laravel-request-docs",
+            demo: "https://kevincobain2000.github.io/laravel-admin-api/request-docs/",
+            blog: ""
+        },
+        {
+            name: "Laravel ERD",
+            description: "Automatically generate Laravel ERD",
+            github: "https://github.com/kevincobain2000/laravel-erd",
+            demo: "https://kevincobain2000.github.io/laravel-blog/erd/",
+            blog: ""
+        },
+        {
+            name: "Miller Rabin Primality Test Online",
+            description: "Big integers prime number checker",
+            github: "https://github.com/kevincobain2000/miller-rabin-primality-test-online",
+            demo: "https://kevincobain2000.github.io/miller-rabin-primality-test-online/",
+            blog: ""
+        },
+        {
+            name: "Google Cloud Icons Search",
+            description: "Search Google Cloud Icons",
+            github: "https://github.com/kevincobain2000/google-cloud-icons",
+            demo: "https://kevincobain2000.github.io/google-cloud-icons/",
+            blog: ""
+        },
+        {
+            name: "Go Language Cheat Sheet",
+            description: "Quick search for golang syntax and features",
+            github: "https://github.com/kevincobain2000/golang-cheat-sheet",
+            demo: "https://kevincobain2000.github.io/golang-cheat-sheet/",
+            blog: ""
+        },
+        {
+            name: "LATEX Writer",
+            description: "Write beautiful docs in LaTeX css.",
+            github: "https://github.com/kevincobain2000/latex-writer/",
+            demo: "https://kevincobain2000.github.io/latex-writer/",
+            blog: ""
+        },
+        {
+            name: "Medium Code Highlight",
+            description: "Add highlighted code to Medium story.",
+            github: "https://github.com/kevincobain2000/medium-code-highlight/",
+            demo: "https://kevincobain2000.github.io/medium-code-highlight/",
+            blog: ""
+        }
+    ];
     return (
         <div>
           <h2>Tools</h2>
           <div className="appearances-list">
             <div className="headshot"></div>
             <ul>
-              <li>
-                <h4>Sentiment Classifier</h4>
-                Sentiment Classifier using Word Sense Disambiguation <br />{" "}
-                <a href="https://github.com/kevincobain2000/sentiment_classifier">
-                  Github
-                </a>{" "}
-                <a
-                  href="https://github.com/kevincobain2000/sentiment_classifier"
-                  title="Sentiment Classifier using WSD"
-                >
-                  <i className="fa fa-play-circle"></i>
-                </a>
-              </li>
-              <li>
-                <h4>jProcessing</h4>
-                Japanese Natural Langauge Processing Libraries <br />{" "}
-                <a href="https://github.com/kevincobain2000/jProcessing">
-                  Github
-                </a>{" "}
-                <a
-                  href="https://github.com/kevincobain2000/jProcessing"
-                  title="Sentiment Classifier using WSD"
-                >
-                  <i className="fa fa-play-circle"></i>
-                </a>
-              </li>
-              <li>
-                <h4>Laravel Throttled Alert Notifications</h4>
-                Send alert to email, microsoft teams from laravel app, when an
-                exception occurs. Throttle is enabled by default.
-                <br />{" "}
-                <a href="https://github.com/kevincobain2000/laravel-alert-notifications">
-                  Github
-                </a>{" "}
-                <a
-                  href="https://github.com/kevincobain2000/laravel-alert-notifications"
-                  title="Sentiment Classifier using WSD"
-                >
-                  <i className="fa fa-play-circle"></i>
-                </a>
-              </li>
-              <li>
-                <h4>Gobrew</h4>
-                Install and manage Go version{" "}
-                <a href="https://github.com/kevincobain2000/gobrew">
-                  Github
-                </a>{" "}
-                |{" "}
-                <a
-                  href="https://medium.com/web-developer/go-version-manager-gobrew-c8750157dfe6"
-                  title="Medium article"
-                >
-                  Medium
-                </a>
-              </li>
-              <li>
-                <h4>Setup Go - Github Action</h4>
-                Dead simple setup up go{" "}
-                <a href="https://github.com/kevincobain2000/action-gobrew">
-                  Github
-                </a>{" "}
-                |{" "}
-                <a
-                  href="https://medium.com/web-developer/dead-simple-go-setup-github-actions-ee9fd7e1c87f"
-                  title="Medium article"
-                >
-                  Medium
-                </a>
-              </li>
-              <li>
-                <h4>Scrape App store and Google play reviews</h4>
-                App store reviews monitoring tool{" "}
-                <a href="https://github.com/kevincobain2000/go-app-reviews-scraper">
-                  Github
-                </a>{" "}
-                |{" "}
-                <a
-                  href="https://github.com/kevincobain2000/go-app-reviews-scraper"
-                  title="Demo"
-                >
-                  Demo
-                </a>
-              </li>
-              <li>
-                <h4>Laravel Request Docs</h4>
-                Automatically generate Laravel API docs{" "}
-                <a href="https://github.com/rakutentech/laravel-request-docs">
-                  Github
-                </a>{" "}
-                |{" "}
-                <a
-                  href="https://kevincobain2000.github.io/laravel-admin-api/request-docs/"
-                  title="laravel request docs demo"
-                >
-                  Demo
-                </a>
-              </li>
-              <li>
-                <h4>Laravel ERD</h4>
-                Automatically generate Laravel ERD{" "}
-                <a href="https://github.com/kevincobain2000/laravel-erd">
-                  Github
-                </a>{" "}
-                |{" "}
-                <a
-                  href="https://kevincobain2000.github.io/laravel-blog/erd/"
-                  title="laravel ERD demo"
-                >
-                  Demo
-                </a>
-              </li>
-              <li>
-                <h4>Miller Rabin Primality Test Online</h4>
-                Big integers prime number checker{" "}
-                <a href="https://github.com/kevincobain2000/miller-rabin-primality-test-online">
-                  Github
-                </a>{" "}
-                |{" "}
-                <a
-                  href="https://kevincobain2000.github.io/miller-rabin-primality-test-online/"
-                  title="Sentiment Classifier using WSD"
-                >
-                  Demo
-                </a>
-              </li>
-              <li>
-                <h4>Google Cloud Icons Search</h4>
-                Search Google Cloud Icons{" "}
-                <a href="https://github.com/kevincobain2000/google-cloud-icons">
-                  Github
-                </a>{" "}
-                |{" "}
-                <a
-                  href="https://kevincobain2000.github.io/google-cloud-icons/"
-                  title="Search Google Cloud Icons"
-                >
-                  Demo
-                </a>
-              </li>
-              <li>
-                <h4>Go Language Cheat Sheet</h4>
-                Quick search for golang syntax and features{" "}
-                <a href="https://kevincobain2000.github.io/golang-cheat-sheet/">
-                  Github
-                </a>{" "}
-                |{" "}
-                <a
-                  href="https://kevincobain2000.github.io/golang-cheat-sheet/"
-                  title="Go Lang cheat sheet tool"
-                >
-                  Demo
-                </a>
-              </li>
-              <li>
-                <h4>LATEX Writer</h4>
-                Write beautiful docs in LaTeX css.{" "}
-                <a href="https://github.com/kevincobain2000/latex-writer/">
-                  Github
-                </a>{" "}
-                |{" "}
-                <a
-                  href="https://kevincobain2000.github.io/latex-writer/"
-                  title="Go Lang cheat sheet tool"
-                >
-                  Demo
-                </a>
-              </li>
-              <li>
-                <h4>Medium Code Highlight</h4>
-                Add highlighted code to Medium story.{" "}
-                <a href="https://github.com/kevincobain2000/medium-code-highlight/">
-                  Github
-                </a>{" "}
-                |{" "}
-                <a
-                  href="https://kevincobain2000.github.io/medium-code-highlight/"
-                  title="Go Lang cheat sheet tool"
-                >
-                  Demo
-                </a>
-              </li>
+                {openSourced.map((appearance) => (
+                    <li key={appearance.name}>
+                        <h4>{appearance.name}</h4>
+                        {appearance.description}
+                        <br />
+                        <a href={appearance.github} title={appearance.github}>Github</a>
+                        {" "}
+                        {appearance.demo && <a href={appearance.demo} title={appearance.demo}>Demo</a>}
+                        {" "}
+                        {appearance.blog && <a href={appearance.blog} title={appearance.blog}>Blog</a>}
+                    </li>
+                ))}
+
             </ul>
           </div>
         </div>

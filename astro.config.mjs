c9d0b443e81ec129d943ed9fd0f93e272d4c67cd
Pulkit Kathuria <kevincobain2000@gmail.com>
2022-08-29 19:44:12 +0900

    added blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,46 @@
-import { defineConfig } from 'astro/config'; // import lit from '@astrojs/lit';
-// https://astro.build/config
-
+import { defineConfig } from 'astro/config';
+import image from "@astrojs/image";
+import mdx from "@astrojs/mdx";
 import compress from "astro-compress";
+import sitemap from "@astrojs/sitemap";
+import getReadingTime from "reading-time";
+import { toString } from "mdast-util-to-string";
 
-// https://astro.build/config
-export default defineConfig({// ...
-  // integrations: [lit()],
+export default defineConfig({
 
   integrations: [
     compress(),
-  ]
+    image(),
+    sitemap(),
+    mdx({
+      remarkPlugins: {
+        extends: [setFallbackLayout, remarkReadingTime],
+      },
+    }),
+  ],
+  markdown: {
+    shikiConfig: {
+      theme: "dracula-soft",
+      wrap: true,
+    },
+  },
 });
+
+
+function setFallbackLayout() {
+  // sets a default layout for all mdx files
+  return function (_tree, file) {
+    const layout =
+      file.data.astro.frontmatter.layout ?? "@layouts/BlogLayout.astro";
+    file.data.astro.frontmatter.layout = layout;
+  };
+}
+
+function remarkReadingTime() {
+  // adds a reading time to all mdx files
+  return function (tree, file) {
+    const textOnPage = toString(tree);
+    const readingTime = getReadingTime(textOnPage);
+    file.data.astro.frontmatter.minutesRead = readingTime.text;
+  };
+}
